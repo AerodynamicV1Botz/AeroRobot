e65858180d85abcc33595abdbd3f7765361ef930
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-06-14 19:04:25 +0530

    Update __main__.py

--- Yone/__main__.py
+++ Yone/__main__.py
@@ -240,8 +240,8 @@ def yone_about_callback(update: Update, context: CallbackContext):
             reply_markup=InlineKeyboardMarkup(
                 [
                    [
-                     InlineKeyboardButton(text="Support", url="t.me/Yone_Support"),
-                     InlineKeyboardButton(text="News", url="t.me/Yone_Updates"),
+                     InlineKeyboardButton(text="Support", url="t.me/AerodynamicV1_Promotion"),
+                     InlineKeyboardButton(text="News", url="t.me/AerodynamicV1_UPDATE"),
                    ],
                    [
                     InlineKeyboardButton(text="Back", callback_data="yone_back")
