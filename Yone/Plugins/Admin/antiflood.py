a992ce73fb148817e7d0d2365f9c9d3a7df25bc4
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-06-29 18:12:58 +0530

    Update antiflood.py

--- Yone/Plugins/Admin/antiflood.py
+++ Yone/Plugins/Admin/antiflood.py
@@ -393,11 +393,11 @@ __help__ = """
 Antiflood allows you to take action on users that send more than x messages in a row. Exceeding the set flood \
 will result in restricting that user.
  This will mute users if they send more than 10 messages in a row, bots are ignored.
- ‣ `/flood`*:* Get the current flood control setting
+ ‣ /flood*:* Get the current flood control setting
 • *Admins only:*
- ‣ `/setflood <int/'no'/'off'>`*:* enables or disables flood control
- *Example:* `/setflood 10`
- ‣ `/setfloodmode <ban/kick/mute/tban/tmute> <value>`*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+ ‣ /setflood <int/'no'/'off'>*:* enables or disables flood control
+ *Example:* /setflood 10
+ ‣ /setfloodmode <ban/kick/mute/tban/tmute> <value>*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
 • *Note:*
  ‣ Value must be filled for tban and tmute!!
  It can be:
