c3f0780b7af05845a652d407b0ce78105647357f
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-06-29 17:49:18 +0530

    Update fun.py

--- Yone/Plugins/Tools/fun.py
+++ Yone/Plugins/Tools/fun.py
@@ -356,21 +356,21 @@ def weebify(update: Update, context: CallbackContext):
 
 
 __help__ = """
- ‣ `/runs`*:* reply a random string from an array of replies
- ‣ `/slap`*:* slap a user, or get slapped if not a reply
- ‣ `/shrug`*:* get shrug XD
- ‣ `/table`*:* get flip/unflip :v
- ‣ `/decide`*:* Randomly answers yes/no/maybe
- ‣ `/toss`*:* Tosses A coin
- ‣ `/bluetext`*:* check urself :V
- ‣ `/roll`*:* Roll a dice
- ‣ `/rlg`*:* Join ears,nose,mouth and create an emo ;-;
- ‣ `/shout <keyword>`*:* write anything you want to give loud shout
- ‣ `/weebify <text>`*:* returns a weebified text
- ‣ `/sanitize`*:* always use this before /pat or any contact
- ‣ `/pat`*:* pats a user, or get patted
- ‣ `/8ball`*:* predicts using 8ball method
- ‣ `/punchme or kickme`*:* punches the user who issued the command from the group
+ ‣ /runs*:* reply a random string from an array of replies
+ ‣ /slap*:* slap a user, or get slapped if not a reply
+ ‣ /shrug*:* get shrug XD
+ ‣ /table*:* get flip/unflip :v
+ ‣ /decide*:* Randomly answers yes/no/maybe
+ ‣ /toss*:* Tosses A coin
+ ‣ /bluetext*:* check urself :V
+ ‣ /roll*:* Roll a dice
+ ‣ /rlg*:* Join ears,nose,mouth and create an emo ;-;
+ ‣ /shout `<keyword>`*:* write anything you want to give loud shout
+ ‣ /weebify `<text>`*:* returns a weebified text
+ ‣ /sanitize*:* always use this before /pat or any contact
+ ‣ /pat*:* pats a user, or get patted
+ ‣ /8ball*:* predicts using 8ball method
+ ‣ /punchme or /kickme*:* punches the user who issued the command from the group
 """
 
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize, run_async=True)
