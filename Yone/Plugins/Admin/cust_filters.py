4e726a2b048fb523f4011dad15d32620324fa2e0
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-06-29 18:06:34 +0530

    Update cust_filters.py

--- Yone/Plugins/Admin/cust_filters.py
+++ Yone/Plugins/Admin/cust_filters.py
@@ -606,27 +606,27 @@ def __chat_settings__(chat_id, user_id):
     return "There are `{}` custom filters here.".format(len(cust_filters))
 
 __help__ = """
- ‣ `/filters`*:* List all active filters saved in the chat.
+ ‣ /filters*:* List all active filters saved in the chat.
 *Admin only:*
- ‣ `/filter <keyword> <reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+ ‣ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
  Separate diff replies by `%%%` to get random replies
  *Example:* 
- `/filter "filtername"
+ /filter `"filtername"
  Reply 1
  %%%
  Reply 2
  %%%
  Reply 3`
- ‣ `/stop <filter keyword>`*:* Stop that filter.
+ ‣ /stop <filter keyword>*:* Stop that filter.
 
 *Chat creator only:*
- ‣ `/removeallfilters`*:* Remove all chat filters at once.
+ ‣ /removeallfilters*:* Remove all chat filters at once.
  
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
-Check ‣ `/markdownhelp` to know more!
+Check ‣ /markdownhelp to know more!
 """
 
 __mod_name__ = "Filters"
